d8ad1988eb1e6da6459ffb172c4cc808df03571f
Lukas Kubis <me@lukaskubis.net>
2018-05-25 08:42:15 +0200

    namespace fix

--- Pages/System/Index.cshtml.cs
+++ Pages/System/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SystemNamespaceIssue.Pages.System
+namespace SystemNamespaceIssue.Pages.SystemStatus
 {
     public class IndexModel : PageModel
     {
